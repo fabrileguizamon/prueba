e969d517422a291e498cc53a19606c0eaf558d66
fabrileguizamon <56940192+fabrileguizamon@users.noreply.github.com>
2019-11-15 00:31:32 -0300

    Add files via upload
    Le agregue un slider

--- hoja.js
+++ hoja.js
@@ -1,20 +1,14 @@
-/* All images belong to its Intellectual Property Owner @ http://burciagraphy.com/ */
+var slideIndex = 0;
+showSlides();
 
-/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-UPDATE: This is absolutely horrible, I have a new respect for the workings of
-        javascript!! Althought, it is one of my favorite languages..
-
-        I suggest you view in a full window becuase i have not yet made images
-        responsive enough, so the images just look fat and stretchy.
-@ 3/19/13
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-UPDATE: Used an image compressor @http://www.smushit.com/ysmush.it/ as well used
-        an html formatter @http://www.freeformatter.com/html-formatter.html.
-        Since load time was INSANE, i improved by getting rid of my CSS only image
-        pre-loader, compressing images, and uplaoded all images to google's image
-        hosting, SAME domain.
-        Made an image sprite for the buttons and logo.
-@ 3/20/2013
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-
-// Props to @scarlson1109: one of his test-cases assisted my solution so far
+function showSlides() {
+       var i;
+       var slides = document.getElementsByClassName("mySlides");
+       for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+       }
+       slideIndex++;
+       if(slideIndex > slides.length) {slideIndex = 1}
+       slides[slideIndex-1].style.display = "block";
+       setTimeout(showSlides,4000);
+}
